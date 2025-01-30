5daa296782053a3504fb75074497c92e92ef4f45
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2025-01-30 21:58:30 +0600

    added templete string

--- practice.js
+++ practice.js
@@ -1,2 +1,11 @@
 const multiply=(a,b,c)=>a*b*c;
 console.log(multiply(5,6,2))
+
+
+// templete string
+const temString=`
+I am a web developer.
+I love to code.
+I love to eat biryani.`
+
+console.log(temString)
