bfcd85f68f0821267be68eacb7b1d0670ba745d2
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2025-01-30 22:44:34 +0600

    added friend named array

--- practice.js
+++ practice.js
@@ -13,4 +13,17 @@ console.log(temString)
 
 // arrow function with default parameter
 const add=(first,second=5)=>first+second;
-console.log(add(4))
+console.log(add(4));
+
+const friend=['Jhinuk'];
+
+for(const cher of friend){
+    if(cher%2===0){
+        console.log('even')
+    }else{
+       
+        const evenLength=[];
+        evenLength.push(cher);
+        console.log(evenLength)
+    }
+}
